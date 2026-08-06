a3ca75f1b979fd0c26d89df6c7e65cc74b902c5b
Patrick McCaffrey <1471088+McCaffeteria@users.noreply.github.com>
2026-08-05 23:52:58 -0400

    Implemented hashmaps and virtual events (no testing)
    Still remaining:
    - synchronize events relationships
    - contemp events to groups
    - find all events/groups with zero priors, add to queue
    - Draw events

--- timelineLogic.js
+++ timelineLogic.js
@@ -5,7 +5,7 @@ let timelineDataEdit = null; //timelineDataEdit is the current working copy of t
 let flatEvents = [];
 let flatTimelines = [];
 let hashedEvents = new Map();
-let hashedtimelines = new Map();
+let hashedTimelines = new Map();
     
 document.getElementById("timeline-button-load-file").addEventListener("click", handleLoadTimeline);
 
@@ -54,12 +54,18 @@ function handleSaveTimeline() { // Saves the current state of timelineData to a 
 }
 
 function buildTimeline() { // Run this any time timelineData has changes that you want to show.
-  // prepData() {
-  // 1. Build a copy of timelineData, and build a hashmap of every event in the copy by ID
-  //   1a. Save the timeline to a timeline hashmap
-  //   1b. For every timeline, place entry events where each is contemp with the previous.
-  //   1c. Place exit events where each is contemporary with the last, and where at least one follows the entry events.
-  //   1d. Place the timeline's internal events also marked as folowing the last entry event and prior to the exit events.
+  // Create working copy of timeline data
+  timelineDataEdit = structuredClone(timelineData);
+  
+  // Make sure all my variables are blank
+  flatEvents = [];
+  flatTimelines = [];
+  hashedEvents.clear();
+  hashedTimelines.clear();
+  
+  // Add all real and virtual events to the hashmap, and handle subtimeline replationships
+  flatRecurse(timelineDataEdit, [null, null]);
+  
   // 2. Synchronise every event relationship with each other in the hashmap
   //   2a. For events that are contemp with a timeline, simply add special relationship rules for follows timelineIDEntryA and prior to timelineIDExitA, and then ad the recipricals to the entry and exit events.
   // 3. Find all events that are contemp, and asign them to groups (create another hashmap where each ID contains a pointer to its group, so that I can look up any event ID and it will either return null if it is not yet asigned a group or it will return the group object).
@@ -76,60 +82,31 @@ function buildTimeline() { // Run this any time timelineData has changes that yo
   // 5. Place a new event, and so on.
   // 6. If the queue is empty and the groups hashmap is not, then you have a circular dependancy. (Optional: Itterate through the remaining events to find which ones have the least remaining priors, and display them to the user to be fixed)
   // }
-  
-  prepData();
-  for (const timeline of flatTimelines) {
-    syncData(getById(timeline.id));
-  }
-  for (const event of flatEvents) {
-    syncData(getById(event.id));
-  }
+
+  //for (const timeline of flatTimelines) {
+  //  syncData(getById(timeline.id));
+  //}
+  //for (const event of flatEvents) {
+  //  syncData(getById(event.id));
+  //}
 }
 
-function prepData() {
-  timelineDataEdit = structuredClone(timelineData);
-  hashedEvents.clear();
-  flatEvents = [];
-  flatTimelines = [];
-  
-  flatRecurse(timelineDataEdit, "timelineDataEdit");
-}
-
-//function flattenData(data) {
-//  flatEvents = [];
-//  flatTimelines = [];
-//  
-//  flatRecurse(data, "timelineData");
-//}
-
-function flatRecurse(node) { // Only gets called one timeline nodes, not Events.
+function flatRecurse(node, parentEntryExit) { // Only gets called one timeline nodes, not Events.
   if (node.id) { // If the timeline has a valid ID:
-    // Save it to the timelines hash map.
-    hashedtimelines.set(node.id, node);
+    //Insert virtual entry/exit events
+    let currentEntryExit = handleVirtualEvents(node, parentEntryExit)
     
-    // Initialize a variable to remember the last event added
-    let lastVirtualNode = null;
-    let currentVirtualNode = null;
+    // Insert real events between them.
+    handleSubtimelineEvents(node, handleVirtualEvents(node, currentEntryExit));
     
-    // For every column in the timeline:
-    for (let c = 0, c < node.width, c++) {
-      // Add new event to the current timeline, and save that object to currentEntryNode
-      currentVirtualNode = node.events[node.events.push({ id: '${node.id} Entry Node ${c}', priors: [], followers: [], contemporaries: [] })-1];
-      
-      // If it isn't the first virtual entry node:
-      if (lastVirtualNode != null) {
-        // add the previous entry node as contemporary
-        currentVirtualNode.contemporaries.push(lastVirtualNode);
-      }
-      
-      // Add event to the hashmap
-      hashedEvents.set(currentVirtualNode.id, currentVirtualNode);
-      
-      // Save finished entry node as last node
-      lastVirtualNode = currentVirtualNode;
+    // Repeat for all timelines inside node
+    for (const timeline of node.timelines) {
+      flatRecurse(timeline, currentEntryExit);
     }
   }
+}
   
+function oldFlatRecurse(node) { // Leftover stuff I haven't replaced yet
   // Set all of the relations for the flat timeline.
   if (Array.isArray(node.priors)) {
     for (let p = 0, p < node.priors.length(), p++) { //const prior of node.priors) { // Needs to be converted to a regular integer for loop, this just gets the actual data object for the property
@@ -147,40 +124,6 @@ function flatRecurse(node) { // Only gets called one timeline nodes, not Events.
     }
   }
   
-  // create entry virtual events, and put them in flatEvents.
-  let entryNodes = [];
-  for (const entry of node.columns) {
-    entryNodes.push({ id: entry + "Entry", type: "entry", column: entry, priors: [], followers: [], contemporaries: [] }); 
-  }
-  for (let i = 0; i < entryNodes.length; i++) {
-    for (const prior of node.priors) {
-      entryNodes[i].priors.push(prior); // Add the timeline's priors to each entry node.
-    }
-    for (let c = 0; c < entryNodes.length; c++) {
-      if (c != i) {
-        entryNodes[c].contemporaries.push(entryNodes[i].id); // Add contemporaries for every entry.
-      }
-    }
-  }
-  flatEvents.concat(entryNodes);
-  
-  // create exit virtual events, and put them in flatEvents.
-  let exitNodes = [];
-  for (const exit of node.columns) {
-    entryNodes.push({ id: exit + "Exit", type: "exit", column: entry, priors: [], followers: [], contemporaries: [] }); 
-  }
-  for (let i = 0; i < exitNodes.length; i++) {
-    for (const prior of node.priors) {
-      exitNodes[i].priors.push(prior); // Add the timeline's priors to each entry node.
-    }
-    for (let c = 0; c < exitNodes.length; c++) {
-      if (c != i) {
-        exitNodes[c].contemporaries.push(exitNodes[i].id); // Add contemporaries for every entry.
-      }
-    }
-  }
-  flatEvents.concat(exitNodes);
-  
   for (let i = 0; i < (Array.isArray(node.timelines) ? node.timelines : []).length; i++) {
     flatRecurse(node.timelines[i], pathString + ".timelines[" + i + "]");
   }
@@ -190,59 +133,137 @@ function flatRecurse(node) { // Only gets called one timeline nodes, not Events.
   }
 }
 
-function getById(id) { // This always returns the first timelineData object matching the ID, and treats timelines as higher priorety than events. Additional matching ID obejcts will be invisible.
-  let entry = flatTimelines.find(x => x.id === id);
-  if (entry) return eval(entry.path);
-  entry = flatEvents.find(x => x.id === id);
-  if (entry) return eval(entry.path);
-  return null;
+function handleVirtualEvents(node, entryExit) { // Returns entryExit. Only gets called one timeline nodes, not Events.
+  // Save current timeline to the timelines hash map.
+  hashedTimelines.set(node.id, node);
+  
+  // Initialize a variable to remember the last event added
+  let lastVirtualEntryNode = null;
+  let lastVirtualExitNode = null;
+  let currentVirtualNode = null;
+  
+  // Set entry and exit to be the parent timeline virtual events if any are passed
+  if (entryExit[0] != null) { lastVirtualEntryNode = entryExit[0]; }
+  if (entryExit[1] != null) { lastVirtualExitNode = entryExit[1]; }
+  
+  // Add virtual entry events
+  // For every column in the timeline:
+  for (let c = 0, c < node.width, c++) {
+    // Add new entry event to the current timeline, and save that object to currentVirtualNode
+    currentVirtualNode = node.events[node.events.push({ id: '${node.id} Entry Node ${c}', type: "entry", priors: [], followers: [], contemporaries: [] })-1];
+    
+    // If it isn't the first virtual entry node:
+    if (lastVirtualEntryNode != null) {
+      // add the previous entry node as contemporary
+      currentVirtualNode.contemporaries.push(lastVirtualEntryNode);
+    }
+    
+    // Add event to the hashmap
+    hashedEvents.set(currentVirtualNode.id, currentVirtualNode);
+    
+    // Save finished entry node as last node
+    lastVirtualEntryNode = currentVirtualNode;
+  }
+  
+  // Add virtual exit events
+  // For every column in the timeline:
+  for (let c = 0, c < node.width, c++) {
+    // Add new exit event to the current timeline, and save that object to currentVirtualNode
+    currentVirtualNode = node.events[node.events.push({ id: '${node.id} Exit Node ${c}', type: "exit", priors: [], followers: [], contemporaries: [] })-1];
+    
+    // If it isn't the first virtual exit node:
+    if (lastVirtualExitNode != null) {
+      // add the previous exit node as contemporary
+      currentVirtualNode.contemporaries.push(lastVirtualExitNode);
+    }
+    
+    // Add last entry as a prior of current event
+    currentVirtualNode.priors.push(lastVirtualEntryNode);
+    
+    // Add event to the hashmap
+    hashedEvents.set(currentVirtualNode.id, currentVirtualNode);
+    
+    // Save finished entry node as last node
+    lastVirtualExitNode = currentVirtualNode;
+  }
+  
+  // Return the last entry and exit events so i can mark real events as between them later
+  return [lastVirtualEntryNode, lastVirtualExitNode];
 }
 
-function syncData(node) {
+function handleSubtimelineEvents(node, entryExitNodes) { // Only gets called one timeline nodes, not Events.
+  // For every event in the current timeline:
+  for (const event of node.events) {
+    // Set entry/exit as Prior/follower
+    event.priors.push(entryExitNodes[0]);
+    event.followers.push(entryExitNodes[1]);
+    
+    // Add event to the hashmap
+    hashedEvents.set(event.id, event);
+  }
+}
+
+function deduplicate() {
+  // For every event in the hashmap:
+  for (const node of hashedEvents.values()) {
+    // Expand the property array into a set (which removes duplicates) and then colapse back into an array
+    if (Array.isArray(node.priors)) {
+      node.priors = [...new Set(node.priors)];
+    }
+    if (Array.isArray(node.followers)) {
+      node.followers = [...new Set(node.followers)];
+    }
+    if (Array.isArray(node.contemporaries)) {
+      node.contemporaries = [...new Set(node.contemporaries)];
+    }
+  }
+}
+
+function syncData(node) { // Old, rewrite this
   // Check to make sure node actually has fields
-  if (!Array.isArray(node.prior)) {
-    node.prior = [];
+  if (!Array.isArray(node.priors)) {
+    node.priors = [];
   }
-  if (!Array.isArray(node.follower)) {
-    node.follower = [];
+  if (!Array.isArray(node.followers)) {
+    node.followers = [];
   }
-  if (!Array.isArray(node.contemporary)) {
-    node.contemporary = [];
+  if (!Array.isArray(node.contemporaries)) {
+    node.contemporaries = [];
   }
   
   // Check priors and assign follower
-  for (const prior of node.prior) {
-    const target = getById(prior);
+  for (const prior of node.priors) {
+    const target = getById(priors);
     if (!target) continue;
-    if (!Array.isArray(target.follower)) {
-      target.follower = [];
+    if (!Array.isArray(target.followers)) {
+      target.followers = [];
     }
-    if (!target.follower.includes(node.id)) {
-      target.follower.push(node.id);
+    if (!target.followers.includes(node.id)) {
+      target.followers.push(node.id);
     }
   }
   
   // Check follower and assign prior
-  for (const follower of node.follower) {
-    const target = getById(follower);
+  for (const follower of node.followers) {
+    const target = getById(followers);
     if (!target) continue;
-    if (!Array.isArray(target.prior)) {
-      target.prior = [];
+    if (!Array.isArray(target.priors)) {
+      target.priors = [];
     }
-    if (!target.prior.includes(node.id)) {
-      target.prior.push(node.id);
+    if (!target.priors.includes(node.id)) {
+      target.priors.push(node.id);
     }
   }
   
   // Check and assign contemporary
-  for (const contemporary of node.contemporary) {
-    const target = getById(contemporary);
+  for (const contemporary of node.contemporaries) {
+    const target = getById(contemporaries);
     if (!target) continue;
-    if (!Array.isArray(target.contemporary)) {
-      target.contemporary = [];
+    if (!Array.isArray(target.contemporaries)) {
+      target.contemporaries = [];
     }
-    if (!target.contemporary.includes(node.id)) {
-      target.contemporary.push(node.id);
+    if (!target.contemporaries.includes(node.id)) {
+      target.contemporaries.push(node.id);
     }
   }
 }
